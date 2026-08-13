a1978b7f3a3a9e2920d1fdacfe7dcaf67deea75a
bosYer1 <sssenan784@gmail.com>
2026-08-14 02:14:17 +0400

    Update map tile layer attribution and URL

--- src/components/map/ClubMap.tsx
+++ src/components/map/ClubMap.tsx
@@ -42,8 +42,8 @@ export function ClubMap({ clubs, activeClubId, onSelectClub }: ClubMapProps) {
       // OpenStreetMap-in istifadə şərtlərinə görə atribusiya mütləqdir.
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> müəllifləri'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> müəllifləri &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
       />
       {clubsWithCoords.map((club) => (
         <ClubMarker key={club.id} club={club} isActive={club.id === activeClubId} onSelect={onSelectClub} />
