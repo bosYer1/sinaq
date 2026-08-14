608fd02f9829262ea53396a022e598e970bc86d3
bosYer1 <sssenan784@gmail.com>
2026-08-15 02:32:16 +0400

    security: enable HSTS for production

--- next.config.js
+++ next.config.js
@@ -21,6 +21,7 @@ const nextConfig = {
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
           { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
           {
             key: 'Permissions-Policy',
             value: 'camera=(), microphone=(), geolocation=(self)',
