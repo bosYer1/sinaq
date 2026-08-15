ba09606279ad008a68ad4ab7d092f1101103c25e
bosYer1 <sssenan784@gmail.com>
2026-08-15 04:19:23 +0400

    security: restrict remote images to GameYer Supabase host

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.supabase.co',
-        pathname: '/storage/v1/object/public/**',
+        hostname: 'uxcedpbumulpheglhlvs.supabase.co',
+        pathname: '/storage/v1/object/public/club-images/**',
       },
     ],
   },
