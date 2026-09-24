770b35b517803e642748b68a578e25d2f9a395a8
bosYer1 <sssenan784@gmail.com>
2026-09-24 17:08:13 +0400

    fix(map): align preview seed with mobile discovery viewport

--- src/components/map/MapPreview.tsx
+++ src/components/map/MapPreview.tsx
@@ -21,7 +21,7 @@ type PreviewSize = {
   height: number;
 };
 
-const INITIAL_PREVIEW_SIZE: PreviewSize = { width: 390, height: 230 };
+const INITIAL_PREVIEW_SIZE: PreviewSize = { width: 390, height: 210 };
 
 function PreviewMarker({
   id,
