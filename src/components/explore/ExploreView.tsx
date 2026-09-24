5b05cecfee9d1a66bb0754dc006b6e68917d725a
bosYer1 <sssenan784@gmail.com>
2026-09-24 16:57:59 +0400

    feat(discovery): reduce default mobile map height

--- src/components/explore/ExploreView.tsx
+++ src/components/explore/ExploreView.tsx
@@ -349,7 +349,7 @@ export function ExploreView({ clubs, view, searchActive }: ExploreViewProps) {
             <section>
               <div
                 data-mobile-list-map-container="true"
-                className={`relative mb-3 overflow-hidden rounded-[18px] [contain:layout_paint_style] ${searchActive ? 'h-[220px] sm:h-[280px]' : 'h-[340px] sm:h-[400px]'}`}
+                className={`relative mb-3 overflow-hidden rounded-[18px] [contain:layout_paint_style] ${searchActive ? 'h-[220px] sm:h-[280px]' : 'h-[300px] sm:h-[360px]'}`}
               >
                 {mobileListMapActive ? renderMapPanel() : <MapPreview clubs={clubsWithDistance} />}
                 {!mobileListMapActive ? (
